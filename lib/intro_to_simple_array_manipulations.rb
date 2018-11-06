3ddc0566556bf7fe3d2a66f514f2e716b8e3756c
Molly McCarron <mccarronmollye@gmail.com>
2018-11-06 11:49:43 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, 2)
-  array.delete_at(2)
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
